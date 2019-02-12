596f70244ac74c3f93104d4a970e463b5646187f
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-12 11:25:33 -0500

    update

---
+++ bamazonCustomer.js
@@ -0,0 +1,74 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var parser = require("body-parser");
+var i;
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "Bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  connection.query("SELECT * FROM products", function (err, results, fields) {
+    if (err) throw err;
+    start();
+});
+})
+
+function start() {
+  connection.query("SELECT * FROM products", function (err, results, fields) {
+    for (let i = 0; i < results.length; i++){
+      console.log("ID: " + (i + 1) + "\n" + "Item Name: " + results[i].product_name + "\n" + "Price: " + results[i].price);
+      console.log("\n");
+    }
+    inquirer
+      .prompt([
+        {
+        name: "ID",
+        type: "input",
+        message: "What is the ID of the item you'd like to buy?(Select by ID#)",
+        },
+        {
+        name: "Units",
+        type: "input",
+        message: "How many would you like to buy?",
+        }
+        ])
+      .then(function(answer) {
+        i = answer.ID - 1;
+        if (answer.Units <= results[i].stock_quantity) {
+          newQ = results[i].stock_quantity - answer.Units;
+          console.log("Enough quantity!");
+          connection.query("UPDATE products SET stock_quantity='" + newQ + "' WHERE item_id='" + answer.ID + "'")
+          inquirer
+          .prompt({
+            name: "choice",
+            type: "list",
+            message: "Purchased item, do you wish to continue?",
+            choices: ["Yes","No"]
+            }).then(function(answer) {
+            if (answer.choice == "Yes"){
+            start();
+            }
+            else {
+            console.log("Goodbye!");
+            }
+          });
+        } else {
+          console.log("Insufficient quantity!");
+        }
+      });
+    })
+  }
---
+++ bamazonManager.js
@@ -0,0 +1,53 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var parser = require("body-parser");
+var i;
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "Bamazon"
+});
+
+function start() {
+  connection.query("SELECT * FROM products", function (err, results, fields) {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View Products For Sale", "View Low Inventory", 
+        "Add to Inventory", "Add New Product"]
+        })
+      .then(function(answer) {
+        if (answer.choice == "View Products For Sale") {
+            for (i = 0; i < 5; i++) {
+                console.log("Item ID: " + results[i].item_ID);
+                console.log("Item Name: " + results[i].product_name);
+                console.log("Item Price: " + results[i].price);
+                console.log("Item Quantity: " + results[i].stock_quantity);
+                console.log("\n");
+            }
+        }
+        else if (answer.choice == "View Low Inventory") {
+            // Checks to see if the amount of items is in stock
+            for (i = 0; i < 5; i++){
+                if (results[i].stock_quantity < 5) {
+                console.log(results[i].product_name);
+                }
+            }   
+        }
+      });
+    })
+  }
+start();
